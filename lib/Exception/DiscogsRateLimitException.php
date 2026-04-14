c4e52a1d47429c4e2853ad5c297cf2616e1823be
megamaced <22581049+megamaced@users.noreply.github.com>
2026-04-14 22:31:03 +0100

    fix: PSR-12 brace style in DiscogsRateLimitException

--- lib/Exception/DiscogsRateLimitException.php
+++ lib/Exception/DiscogsRateLimitException.php
@@ -7,4 +7,6 @@ namespace OCA\Crate\Exception;
 /**
  * Thrown when the Discogs API responds with HTTP 429 Too Many Requests.
  */
-class DiscogsRateLimitException extends \RuntimeException {}
+class DiscogsRateLimitException extends \RuntimeException
+{
+}
